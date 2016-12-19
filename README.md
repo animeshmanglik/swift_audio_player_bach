d69f2fb09923e8d5e9e7c151279010ba7bb28732
Animesh Manglik <amanglik@whalerockindustries.com>
2016-12-19 15:54:36 -0800

    Swift: Audio Player playing Bach music

---
+++ README.md
@@ -0,0 +1,11 @@
+# Swift Development
+
+## Audio Player Bach
+
+Using concepts of
+
+- AVFoundation
+
+Audio Player
+
+- Shows a basic audio player app with simple controls
--- audio player/ViewController.swift
+++ audio player/ViewController.swift
@@ -7,12 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    
+    var timer = Timer()
+    
+    internal func updatePlayBar() {
+        playBar.value = Float(player.currentTime)
+    }
+    
+    @IBAction func playButton(_ sender: AnyObject) {
+        player.play()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updatePlayBar), userInfo: nil, repeats: true)
+
+    }
+    
+    @IBAction func pauseButton(_ sender: AnyObject) {
+        player.pause()
+        timer.invalidate()
+    }
+    
+    @IBAction func stopButton(_ sender: AnyObject) {
+        playBar.value = 0
+        timer.invalidate()
+        player.pause()
+        player.currentTime = 0
+    }
+    
+    @IBOutlet weak var playBar: UISlider!
+    
+    @IBAction func playBarSlider(_ sender: AnyObject) {
+    
+        player.currentTime = TimeInterval(playBar.value)
+        
+    }
+    
+    @IBOutlet weak var volumeBar: UISlider!
+    
+    @IBAction func volumeBarSlider(_ sender: AnyObject) {
+        
+        player.volume = volumeBar.value
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = Bundle.main.path(forResource: "bach_sheep", ofType: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            playBar.maximumValue = Float(player.duration)
+        } catch {
+            
+            // Print any errors
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
